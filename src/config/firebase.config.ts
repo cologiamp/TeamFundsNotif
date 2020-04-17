033f9751908db186a914b69e5369257ceeca72d3
ignacioviveo <ignacio@viveogroup.com>
2020-04-17 13:02:37 +0300

    first commit

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,10 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDE26LDTVlE4OZR4McZO2EsvrZCerQaSTE",
+    authDomain: "teamfunds-51768.firebaseapp.com",
+    databaseURL: "https://teamfunds-51768.firebaseio.com",
+    projectId: "teamfunds-51768",
+    storageBucket: "teamfunds-51768.appspot.com",
+    messagingSenderId: "361820351905",
+    appId: "1:361820351905:web:62ec25b9925fa37fb2efe0",
+    measurementId: "G-3LG41FE2JJ"
+};
